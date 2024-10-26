5a5e69e0aee10ecf6b88e8401707db9f8794d273
Chau Cao <averagechau@gmail.com>
2024-10-26 13:47:53 +0700

    complete firebase and app config

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:cabquiz/firebase_options.dart';
+import 'package:cabquiz/resources/app_colors.dart';
+import 'package:cabquiz/resources/fonts.gen.dart';
 import 'package:cabquiz/routes/app_router.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -19,11 +28,48 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router.config(),
-      title: 'Cab Quiz',
-      theme: ThemeData(
-        useMaterial3: false,
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
+      builder: (context, child) => MaterialApp.router(
+        routerConfig: router.config(),
+        title: 'Cab Quiz',
+        theme: ThemeData(
+          fontFamily: FontFamily.nunito,
+          textTheme: TextTheme(
+            bodyMedium: TextStyle(
+              fontSize: 16.sp,
+              fontWeight: FontWeight.bold,
+              color: AppColors.greyScale900,
+            ),
+            bodyLarge: TextStyle(
+              fontSize: 20.sp,
+              fontWeight: FontWeight.bold,
+              color: AppColors.greyScale900,
+            ),
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            hintStyle: TextStyle(
+              fontSize: 20.sp,
+              fontWeight: FontWeight.bold,
+              color: AppColors.greyScale500,
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              textStyle: TextStyle(
+                fontSize: 16.sp,
+                fontWeight: FontWeight.bold,
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(100.r),
+              ),
+            ),
+          ),
+          colorScheme: const ColorScheme.light(
+            primary: AppColors.primary,
+          ),
+          useMaterial3: false,
+        ),
       ),
     );
   }
